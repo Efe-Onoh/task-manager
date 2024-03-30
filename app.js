9f57e1317b6c2c9ab8481eeab8262eb9e1b9bffe
Efe O <ednaellenpope@gmail.com>
2024-03-30 19:21:45 +0400

    impl minor

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
-
-const express = require('express') //import express and initialize
-const app = express()
+const express = require("express"); //import express and initialize
+const app = express();
 
 //importing router tasks.js
-const tasks = require('./routes/tasks')
+const tasks = require("./routes/tasks");
 
 //import connection functionality from connect
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
 
-require('dotenv').config()
-const notFound = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //middleware
-app.use(express.static('./public'))//serve up static files
-app.use(express.json()) //so we can access json data in req body when creating tasks
+app.use(express.static("./public")); //serve up static files
+app.use(express.json()); //so we can access json data in req body when creating tasks
 
-//routes 
+//routes
 //based on app, we get tasks so a get request is needed
 //we create tasks so post is needed
 //we can delete tasks, so delete is needed
 //we can edit so update is needed
 //we need to get a single task as well
 
-app.use('/api/v1/tasks', tasks) //make use of router
+app.use("/api/v1/tasks", tasks); //make use of router
 
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-app.use(notFound)
-app.use(errorHandlerMiddleware)
+const port = parseInt(process.env.PORT) || 8080;
 
+const start = async () => {
+  //handles in bg while rest of sequential code is run
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const port = parseInt(process.env.PORT) || 8080
-
-const start = async () =>{ //handles in bg while rest of sequential code is run
-    try{
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`server is listening on port ${port}...`))
-
-    }catch(error){
-        console.log(error)
-
-    }
-}
-
-start()
+start();
